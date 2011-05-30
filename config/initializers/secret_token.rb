d3c8c57ee96627cfe3299f9da10b1417c94a3838
Richard Dean <richardadamdean@gmail.com>
2011-05-31 08:06:49 +1000

    Adding rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningGit::Application.config.secret_token = '0e7e35d171de4675777edc7175d58ec5f7b9ec933d3cd16fe830c41f9e5684db6c8292dcc7ec94f494242ad342005fbd09057d68c3e5536503b95815b5f4c118'
